9c11f8a8ef9b96da5b58d957a1dd43251adb8258
Emmanuel Olusunmade <eolusunmade@outlook.com>
2024-10-10 16:07:42 +0100

    Update main.tf with variables

--- main.tf
+++ main.tf
@@ -8,9 +8,11 @@ terraform {
 }
 
 # Launches an EC2 instance using the specified AMI and instance type.
-resource "aws_instance" "initial_instance_ubuntu" {
-  ami           = "ami-0d64bb532e0502c46" # Using the AMI ID variable from variables.tf
-  instance_type = "t2.micro"       # Using instance type variable
+resource "aws_instance" "Ubuntu1" {
+  ami           = var.ami_id # Using the AMI ID variable from variables.tf
+  instance_type = var.instance_type     # Using instance type variable
   tags = {
-    Name = "initial_ubuntu_instance"
+    Name        = var.instance_name
+    environment = var.environment
   }
+}
